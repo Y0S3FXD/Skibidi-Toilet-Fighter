faecdbcdfda2edc11e3643c72ef542f63eb6ea47
Y0S3FXD <shekyousef@gmail.com>
2023-09-13 14:52:12 +0200

    Its pissing slowly now
    Pissing slowly, but cant collide with another object, something "is trigger on"

--- Assets/Scripts/Piss.cs
+++ Assets/Scripts/Piss.cs
@@ -1,7 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class Piss : Attacks
+
+public class Piss : MonoBehaviour
 {
- public tranform Piss;
+    public Transform PissSpawnPoint;
+    public GameObject PissPreFap;
+    public float PissSpeed = 1.0f;
+    public float SpawnRate = 0.1f; // Adjust this to control the rate of spawning.
+
+    private float timeSinceLastSpawn = 0f;
+
+    private void Update()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+
+        // Check if it's time to spawn a new "piss" object.
+        if (timeSinceLastSpawn >= SpawnRate)
+        {
+            SpawnPiss();
+            timeSinceLastSpawn = 0f; // Reset the timer.
+        }
+    }
+
+    private void SpawnPiss()
+    {
+        var pisso = Instantiate(PissPreFap, PissSpawnPoint.position, PissSpawnPoint.rotation);
+        pisso.GetComponent<Rigidbody>().velocity = PissSpawnPoint.forward * PissSpeed;
+    }
 }
