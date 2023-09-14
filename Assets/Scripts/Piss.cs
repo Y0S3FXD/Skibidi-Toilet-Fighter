1a7221ba63a79a7888b8c56186e3f8234854efd7
Y0S3FXD <shekyousef@gmail.com>
2023-09-14 14:00:54 +0200

    Changes
    stamina now inherite from piss and piss inherit from attacks.
    also now it can detect who hits who, but need to fix stamina dissapearing immiditaly and remeber you used tags in unity editor

--- Assets/Scripts/Piss.cs
+++ Assets/Scripts/Piss.cs
@@ -2,30 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Piss : MonoBehaviour
+public class Piss : Attacks
 {
     public Transform PissSpawnPoint;
     public GameObject PissPreFap;
-    public float PissSpeed = 1.0f;
-    public float SpawnRate = 0.1f; // Adjust this to control the rate of spawning.
-
-    private float timeSinceLastSpawn = 0f;
+    public float PissSpeed = 23.5f;
 
     private void Update()
     {
-        timeSinceLastSpawn += Time.deltaTime;
-
-        // Check if it's time to spawn a new "piss" object.
-        if (timeSinceLastSpawn >= SpawnRate)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            SpawnPiss();
-            timeSinceLastSpawn = 0f; // Reset the timer.
+            for (int i = 0; i < 10; i++)
+            {
+                var pisso = Instantiate(PissPreFap, PissSpawnPoint.position, PissSpawnPoint.rotation);
+                Rigidbody pissRigidbody = pisso.GetComponent<Rigidbody>();
+                pissRigidbody.velocity = PissSpawnPoint.forward * PissSpeed;
+            }
         }
     }
 
-    private void SpawnPiss()
+    // Handle collisions with any object and destroy the "piss" object.
+    private void OnCollisionEnter(Collision collision)
     {
-        var pisso = Instantiate(PissPreFap, PissSpawnPoint.position, PissSpawnPoint.rotation);
-        pisso.GetComponent<Rigidbody>().velocity = PissSpawnPoint.forward * PissSpeed;
+        Destroy(gameObject); // Destroy the "piss" object upon collision with any object.
     }
 }
--- Assets/Scripts/Stamina.cs
+++ Assets/Scripts/Stamina.cs
@@ -1,24 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
-public class Stamina : MonoBehaviour
+public class Stamina : Piss
 {
-    // The time it takes for the ball to disappear.
+    // The time it takes for the object to disappear.
     private float LifeTime = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    // Handle collisions with other objects.
+    private void OnCollisionEnter(Collision collision)
     {
-        if (Time.time > LifeTime)
+        if (collision.gameObject.CompareTag("Enemy1"))
         {
-            Destroy(this);
+            Debug.Log("saloka hit an enemy: " + collision.gameObject.name);
+            // Add your code to handle the collision with an enemy here.
+            Destroy(gameObject); // Destroy the GameObject upon collision with any object.
+
         }
+        else
+        {
+            Debug.Log("Stamina hit something else: " + collision.gameObject.name);
+        }
+
     }
 }
