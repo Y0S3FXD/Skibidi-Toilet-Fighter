85b1f5c8739c8e8b52c14e00e0eb6f85f0febea4
Y0S3FXD <shekyousef@gmail.com>
2023-12-18 09:54:55 +0100

    Piss
    Piss CoolDown https://www.youtube.com/watch?v=47ZkulgnadI

--- Assets/Scripts/Piss.cs
+++ Assets/Scripts/Piss.cs
@@ -1,29 +1,52 @@
-using System.Collections;
-using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using UnityEngine.Events;
 using UnityEngine;
 
-public class Piss : Attacks
+public class Piss : Stamina
 {
+    public float PissCooldown;
+    private float CurrentCooldown;
+    public bool automatic;
     public Transform PissSpawnPoint;
     public GameObject PissPreFap;
     public float PissSpeed = 0.5f;
+    public UnityEvent onshoot;
 
+    private void Start()
+    {
+        CurrentCooldown = PissCooldown;
+    }
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (automatic)
         {
-            for (int i = 0; i < 10; i++)
+            if (Input.GetMouseButton(0))
+            {
+                if (CurrentCooldown <= 0)
+                {
+                    CurrentCooldown = PissCooldown;
+                    onshoot?.Invoke();
+                }
+                else
+                {
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        if (CurrentCooldown <= 0)
+                        {
+                            CurrentCooldown = PissCooldown;
+                            onshoot?.Invoke();
+                        }
+                    }
+                }
+            }
+            /*for (int i = 0; i < 10; i++)
             {
                 var pisso = Instantiate(PissPreFap, PissSpawnPoint.position, PissSpawnPoint.rotation);
                 Rigidbody pissRigidbody = pisso.GetComponent<Rigidbody>();
                 pissRigidbody.velocity = PissSpawnPoint.forward * PissSpeed;
-            }
+            }*/
+            CurrentCooldown -= Time.deltaTime;
         }
-    }
-
-    // Handle collisions with any object and destroy the "piss" object.
-    private void OnCollisionEnter(Collision collision)
-    {
-        Debug.Log("im gonna remove you next time");
     }
 }
