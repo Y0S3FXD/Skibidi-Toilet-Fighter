50537abe7d13bab9f2173c4814f3409605db71a3
Y0S3FXD <shekyousef@gmail.com>
2024-05-15 04:55:01 +0200

    Cleaning

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,13 +6,11 @@ public class GameController : MonoBehaviour
 {
     public GameObject playerOnePrefab; // Player one prefab
     public GameObject playerTwoPrefab; // Player two 
-    private Arena arenaInstance; // Instantiated arena
     public List<GameObject> Maps = new List<GameObject>(); 
     void Start()
     {
         // Instantiate the arena
         GameObject arenaGO = Instantiate(Maps[Random.Range(0,Maps.Count)], Vector3.zero, Quaternion.identity);
-        arenaInstance = arenaGO.GetComponent<Arena>();
         // Instantiate players
         Instantiate(playerOnePrefab, new Vector3(-2, 0, 0), Quaternion.identity); // Position for player one
         Instantiate(playerTwoPrefab, new Vector3(2, 0, 0), Quaternion.identity); // Position for player two
