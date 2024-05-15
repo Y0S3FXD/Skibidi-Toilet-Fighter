520aff9b6f9decdc1481cd72e4afd68cd13412b8
Y0S3FXD <shekyousef@gmail.com>
2024-05-15 04:52:57 +0200

    Collider Now Made to Mesh Colllider

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public GameObject arenaPrefab; // Arena prefab
     public GameObject playerOnePrefab; // Player one prefab
     public GameObject playerTwoPrefab; // Player two 
     private Arena arenaInstance; // Instantiated arena
@@ -14,9 +13,6 @@ public class GameController : MonoBehaviour
         // Instantiate the arena
         GameObject arenaGO = Instantiate(Maps[Random.Range(0,Maps.Count)], Vector3.zero, Quaternion.identity);
         arenaInstance = arenaGO.GetComponent<Arena>();
-        arenaInstance.CreateFloor(Vector3.zero);
-        arenaInstance.CreateLight(new Vector3(0f, 10f, 0f));
-
         // Instantiate players
         Instantiate(playerOnePrefab, new Vector3(-2, 0, 0), Quaternion.identity); // Position for player one
         Instantiate(playerTwoPrefab, new Vector3(2, 0, 0), Quaternion.identity); // Position for player two
